e0553c9d6bca001f0d0b57de112bfdbc1fe70266
Oskar Plichta <oskar.plichta22@gmail.com>
2026-07-16 13:55:27 +0200

    Updated to react 19

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,9 @@ import App from './App.jsx'
 import store from './redux/store'; 
 
 createRoot(document.getElementById('root')).render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-); 
-{/* <StrictMode>  </StrictMode> */}
+    <StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </StrictMode>
+);
